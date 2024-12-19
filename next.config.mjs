3583d27899171c5784b9e5b18c0012e6540b0612
Yousef_rawag <yousefrawag908@gmail.com>
2024-12-19 17:15:20 +0200

    nmr

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['images.ctfassets.net'],
+        unoptimized:true
       },
+      output:"standalone"
+      
 };
 
 export default nextConfig;
