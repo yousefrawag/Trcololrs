5e26ac325083d81426f4b85b8741fe26c391d303
yousefrawag <yousefrawag908@gmail.com>
2025-02-14 23:32:26 +0200

    set api domine

--- services/axiosAuthfetch.js
+++ services/axiosAuthfetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authFetch = axios.create({
-    baseURL:"http://localhost:3300/api" /* "http://localhost:3300/api" */,
+    baseURL:"https://trcolors.com/api" /* "http://localhost:3300/api" */,
 })
 
 authFetch.interceptors.request.use((request) => {
